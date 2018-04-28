4964179572af26a55b2ca1965a9c9d9faf8ed432
plengeh22 <38558176+plengeh22@users.noreply.github.com>
2018-04-28 17:16:18 +0700

    Update safelink-3-multi.js

--- safelink-3-multi.js
+++ safelink-3-multi.js
@@ -27,7 +27,7 @@ if(x=="3"){xxx="how-to-read-forex-chart-properly.html"}
 if(x=="4"){xxx="auto-insurance.html"}
 if(x=="5"){xxx="ways-to-get-cheapest-health-insurance.html"}
 if(x=="6"){xxx="wellbore-stability-while-drilling.html"}
-if(x=="7"){xxx="underlying-strata-failure-due-to-mining.html"}a_to_ve[i].href="https://ratertip.blogspot.com/2018/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="7"){xxx="underlying-strata-failure-due-to-mining.html"}a_to_ve[i].href="https://ratertip.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
